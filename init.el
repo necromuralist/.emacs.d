6c783705d339bf7afd6f747a69fd48b698dfba32
necromuralist <necromuralist@gmail.com>
2018-03-22 16:10:27 -0700

    Removed package-initialize call, because emacs said to

--- init.el
+++ init.el
@@ -8,9 +8,6 @@
     (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
     (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
   
-    ;; activate the packages
-    (package-initialize)
-  
     ;; refresh the list
     (when (not package-archive-contents)
       (package-refresh-contents))
