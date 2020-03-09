1e4a6a64a24742a21e1b2c55fa452931bc50f972
necromuralist <necromuralist@protonmail.com>
2020-03-08 18:42:30 -0700

    Updated the ein settings

--- init.el
+++ init.el
@@ -433,3 +433,16 @@
   ;; )
   ;; 
   ;;
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(ein magit elpy jupyter simplenote2 ox-nikola ob-ipython god-mode deft ac-js2)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
